ce8ebdd9fa609746ad8b899a0cbea80b747c045f
TomPeas <40696788+TomPeas@users.noreply.github.com>
2018-09-27 14:28:16 +0100

    Further Refinement
    Further removed uneccesary items from the 'td' list and began to short the list of needed 'td's into catergories such as item numbers, item names and item prices

--- San_ta_Menu.py
+++ San_ta_Menu.py
@@ -5,15 +5,21 @@ from bs4 import BeautifulSoup
 san_url = 'http://www.san-restaurant.com/san-finest-chinese-cuisine-take-away-menu.html'
 the_page = urllib.request.urlopen(san_url)
 soup = BeautifulSoup(the_page,'html.parser')
-
+counter = 0
 html_headers =  soup.findAll('h3')
 headers = []
 for each in html_headers[2:-3]:
     headers.append(each.text)
 html_dishes_unrefined = soup.findAll('td')
-html_dishes = []
-for each in html_dishes[32::]:
+html_dishes_refined = []
+for each in html_dishes_unrefined[32:-24]:
+    html_dishes_refined.append(each)
 
 
+for each in html_dishes_refined[2::3]: # finds the item numbers
 
 
+for each in html_dishes_refined[::3]: #finds the item names
+    print(each.text)
+
+for each in html_dishes_refined[1::3]: #find the item prices
