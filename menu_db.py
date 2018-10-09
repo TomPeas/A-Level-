7868651fe8b18928887f548b42e28d7a007ba313
TomPeas <40696788+TomPeas@users.noreply.github.com>
2018-10-09 14:16:30 +0100

    Created Database Class
    created the OO datebase class and added simple funtions to create tables on the database and edit the tables

--- menu_db.py
+++ menu_db.py
@@ -4,3 +4,19 @@ import sql
 with open('menu_details', 'rb') as infile:
     menu = pickle.load(infile)
     menu_prices = pickle.load(infile)
+
+
+class Database:
+    def __init__(self, db_name):
+        with sql.connect(db_name) as self.connect:
+            self.cursor = self.connect.cursor()
+
+    def create_table(self, sql):
+        self.cursor.excecute(sql)
+        self.connect.commit()
+
+    def update_table(self, sql):
+        self.cursor.excecute(sql)
+        self.connect.commit()
+
+
