1a80c763b35171d8066b40cdabfc12848326e031
TomPeas <40696788+TomPeas@users.noreply.github.com>
2018-10-18 12:48:27 +0100

    DB SQL
    Created 3 SQLs to create 3 tables on the data base to store the menu items

--- menu_db.py
+++ menu_db.py
@@ -19,4 +19,8 @@ class Database:
         self.cursor.excecute(sql)
         self.connect.commit()
 
+create_dishes_sql = ''' CREATE TABLE IF NOT EXISTS Santa_Dishes ( DishID text PRIMARY KEY, Dish text NOT NULL, Section text NOT NULL ) '''
+create_prices_sql = ''' CREATE TABLE IF NOT EXISTS Santa_Prices ( PriceID text PRIMARY KEY, Price real NOT NULL ) '''
+create_menu_sql = ''' CREATE TABLE IF NOT EXISTS Santa_Menu (ItemID text PRIMARY KEY, DishID text FOREIGN KEY, PriceID text FOREIGN KEY ) '''
+
 
